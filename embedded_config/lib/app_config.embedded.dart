947f211a3d62d992b541597c5e8aa3e518328d22
Alp Konca <alp.konca@kobil.com>
2023-04-26 13:52:23 +0300

    demo successful

---
+++ embedded_config/lib/app_config.embedded.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of app_config;
+
+// **************************************************************************
+// ConfigGenerator
+// **************************************************************************
+
+class _$AppConfigEmbedded extends AppConfig {
+  const _$AppConfigEmbedded();
+
+  @override
+  final apiUrl = 'deneme';
+}
